140a9159756da056ac3408af88627a72e06344e4
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2026-03-05 21:24:39 +0800

    uncomment

--- browser/user.js
+++ browser/user.js
@@ -81,27 +81,26 @@ user_pref("widget.windows.mica", true)
 // user_pref("zen.theme.content-element-separation", 0)
 
 // Telemetry - https://github.com/K3V1991/Disable-Firefox-Telemetry-and-Data-Collection
-// user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
-// user_pref("browser.newtabpage.activity-stream.telemetry", false);
-// user_pref("browser.ping-centre.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("browser.ping-centre.telemetry", false);
 // user_pref("datareporting.healthreport.service.enabled", false);
-// user_pref("datareporting.healthreport.uploadEnabled", false);
-// user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
 // user_pref("datareporting.sessions.current.clean", true);
 // user_pref("devtools.onboarding.telemetry.logged", false);
-// user_pref("toolkit.telemetry.archive.enabled", false);
-// user_pref("toolkit.telemetry.bhrPing.enabled", false);
-// user_pref("toolkit.telemetry.enabled", false);
-// user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
 // user_pref("toolkit.telemetry.hybridContent.enabled", false);
-// user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
 // user_pref("toolkit.telemetry.prompted", 2);
 // user_pref("toolkit.telemetry.rejected", true);
 // user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
-// user_pref("toolkit.telemetry.server", "");
-// user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
-// user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
 // user_pref("toolkit.telemetry.unifiedIsOptIn", false);
-// user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
 
 // https://github.com/yokoffing/Betterfox
